e06082b9c20dd0e9f6f7fe3753fe6c79260653fa
MIZiper <miziper@163.com>
2025-01-30 07:22:23 +0800

    personlist layout

--- frontend/src/components/PersonList.vue
+++ frontend/src/components/PersonList.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
-    <v-btn @click="openDialog">添加人员</v-btn>
+    <v-btn icon class="fab" @click="openDialog" color="primary">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
     <v-dialog v-model="dialog" max-width="500px">
       <v-card>
         <v-card-title>
@@ -22,12 +24,15 @@
       </v-card>
     </v-dialog>
     <v-list>
-      <v-list-item v-for="(person, index) in people" :key="index">
+      <v-list-item v-for="(person, index) in people" :key="index" class="hover-item">
+        <v-list-item-avatar>
+          <v-img :src="person.photo || defaultPhoto" max-width="50" max-height="50"></v-img>
+        </v-list-item-avatar>
         <v-list-item-content>
           <v-list-item-title>{{ person.name }}</v-list-item-title>
           <v-list-item-subtitle>{{ person.contact }}</v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-action>
+        <v-list-item-action class="hover-actions">
           <v-btn icon @click="editPerson(index)">
             <v-icon>mdi-pencil</v-icon>
           </v-btn>
@@ -52,8 +57,12 @@ export default {
         contact: '',
         description: ''
       },
-      people: JSON.parse(localStorage.getItem('people') || '[]')
+      people: JSON.parse(localStorage.getItem('people') || '[]'),
+      defaultPhoto: 'path/to/default/photo.png'
     };
+  },
+  created() {
+    this.loadPeople();
   },
   methods: {
     openDialog() {
@@ -66,13 +75,28 @@ export default {
     },
     savePerson() {
       if (this.$refs.form.validate()) {
-        if (this.editIndex === -1) {
-          this.people.push({ ...this.person });
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.person.photo = e.target.result;
+          if (this.editIndex === -1) {
+            this.people.push({ ...this.person });
+          } else {
+            this.people.splice(this.editIndex, 1, { ...this.person });
+          }
+          localStorage.setItem('people', JSON.stringify(this.people));
+          this.closeDialog();
+        };
+        if (this.person.photo) {
+          reader.readAsDataURL(this.person.photo);
         } else {
-          this.people.splice(this.editIndex, 1, { ...this.person });
+          if (this.editIndex === -1) {
+            this.people.push({ ...this.person });
+          } else {
+            this.people.splice(this.editIndex, 1, { ...this.person });
+          }
+          localStorage.setItem('people', JSON.stringify(this.people));
+          this.closeDialog();
         }
-        localStorage.setItem('people', JSON.stringify(this.people));
-        this.closeDialog();
       }
     },
     editPerson(index) {
@@ -83,7 +107,49 @@ export default {
     deletePerson(index) {
       this.people.splice(index, 1);
       localStorage.setItem('people', JSON.stringify(this.people));
+    },
+    loadPeople() {
+      this.people = JSON.parse(localStorage.getItem('people') || '[]');
     }
   }
 };
 </script>
+
+<style scoped>
+.v-list-item {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  min-height: 100px;
+  display: flex;
+  align-items: center;
+}
+.v-list-item-avatar {
+  margin-right: 16px;
+}
+.v-img {
+  max-width: 50px;
+  max-height: 50px;
+}
+.hover-item {
+  position: relative;
+}
+.hover-actions {
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+.hover-item:hover .hover-actions {
+  opacity: 1;
+}
+.fab {
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
